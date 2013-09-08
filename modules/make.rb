ee9ad3e91e26ac4a9fcfcf9940d44553bb34f72b
Andrea Marchesini <baku@ippolita.net>
2013-09-08 10:42:25 +0200

    compile messages for each terminal

--- modules/make.rb
+++ modules/make.rb
@@ -1,6 +1,6 @@
 require 'gtk2'
 
-@current_terminal = nil
+@terminals = {}
 
 @compiling_sentences = [
   "It's always fun to see code compiling",
@@ -12,25 +12,31 @@ require 'gtk2'
   "Compilation is finished"
 ];
 
-#def do_stuff(terminal)
-#  return if @current_terminal.nil? or
-#            @current_terminal[:terminal] != terminal or
-#            @current_terminal[:compiling] == false
+#def do_stuff(pid, terminal)
+#  return if not @terminals.include? pid or
+#            @terminals[:compiling] == false
 #  line = terminal.get_text(get_attrs=false).split("\n").last
 #  puts line
 #end
 
 def terminal_new(terminal, pid)
+  @terminals[pid] = { :compiling => false }
 #  terminal.signal_connect("text-inserted") do |widget|
-#    do_stuff widget
+#    do_stuff pid
 #  end
 end
 
 def terminal_show(terminal, pid)
-  @current_terminal = { :terminal => terminal, :pid => pid.to_i, :compiling => false }
+  if @terminals[pid][:compiling]
+    @@bterm.notification.show @compiling_sentences.sample
+  end
 end
 
-def is_make_running(what)
+def terminal_close(terminal, pid)
+  @terminals.delete(pid)
+end
+
+def is_make_running(pid, what)
   number = true if Float(what) rescue false
   return false if number != true
 
@@ -40,7 +46,7 @@ def is_make_running(what)
   file = File.new("/proc/#{what}/status", 'r')
   while line = file.gets do
     if line.start_with? 'PPid:'
-      return line.split[1].to_i == @current_terminal[:pid]
+      return line.split[1].to_i == pid
     end
   end
 
@@ -48,19 +54,19 @@ def is_make_running(what)
 end
 
 GLib::Timeout.add 1000 do
-  if not @current_terminal.nil?
-
+  @terminals.each do |pid, data|
     compiling = false
 
     Dir.entries('/proc').each do |dir|
-      if is_make_running dir
+      running = is_make_running pid, dir rescue false
+      if running
         compiling = true
         break
       end
     end
 
-    if compiling != @current_terminal[:compiling]
-      @current_terminal[:compiling] = compiling
+    if compiling != data[:compiling]
+      data[:compiling] = compiling
 
       if compiling
         @@bterm.notification.show @compiling_sentences.sample
@@ -69,8 +75,10 @@ GLib::Timeout.add 1000 do
       end
     end
   end
+
   true
 end
 
 @@bterm.register_hooks :terminal_new, method(:terminal_new)
 @@bterm.register_hooks :terminal_show, method(:terminal_show)
+@@bterm.register_hooks :terminal_close, method(:terminal_close)

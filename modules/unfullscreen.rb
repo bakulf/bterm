6b33aa824d1909806397113d8445d38b5dfbe24f
ziomizar <giuseppe.piraino@gmail.com>
2014-11-03 23:15:23 +0000

    Update unfullscreen.rb

--- modules/unfullscreen.rb
+++ modules/unfullscreen.rb
@@ -1,6 +1,31 @@
-# Remove fullscreen from bterm
+# Set the bterm to half width
+# change between width and height require a double click
+# of key combination
+def unfullscreen_height
+  # At the moment this is the best way i found 
+  # to access @window object. With the hook :terminal_new 
+  # i can access the terminal object but not the @window
+  window = @@bterm.instance_variable_get(:@window)
 
-def unfullscreen
+  window.decorated = true
+  window.set_resizable(true)
+
+  # Resize the width of the window
+  width = window.screen.width
+  height = window.screen.height / 2
+  window.unfullscreen
+
+  # We need to change the minimum size of the window
+  # then we can resize to a smaller size 
+  window.set_size_request(width, height)
+  window.move(0, 0)
+  window.resize(width, height)
+end
+
+# Set the bterm to half height
+# change between width and height require a double click
+# of key combination
+def unfullscreen_width
   # At the moment this is the best way i found 
   # to access @window object. With the hook :terminal_new 
   # i can access the terminal object but not the @window
@@ -22,7 +47,7 @@ def unfullscreen
 end
 
 
-# Restore fullwidth to bterm
+# Restore fullwidth for bterm
 
 def fullscreen
   # For the moment this is the best way i found 
@@ -42,3 +67,4 @@ def fullscreen
   window.move(0,0)
   window.fullscreen
 end
+

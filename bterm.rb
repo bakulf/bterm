e52cdd1c95d45e448c18c829c4de23f0313cd677
Andrea Marchesini <baku@ippolita.net>
2015-06-22 10:11:11 +0100

    Mask for ruby-gtk2 updates

--- bterm.rb
+++ bterm.rb
@@ -483,23 +483,30 @@ private
     @window.add_accel_group(ag)
   end
 
+  def set_mask(mask, what)
+    if mask.nil?
+      return what
+    end
+    return mask | what
+  end
+
   def parse_hotkey(str)
-    mask = 0
+    mask = nil
     char = nil
 
     str.split.each do |p|
       p.strip!
 
       if p.downcase == 'ctrl'
-        mask |= Gdk::Window::CONTROL_MASK
+        mask = set_mask(mask, Gdk::Window::CONTROL_MASK)
       elsif p.downcase == 'shift'
-        mask |= Gdk::Window::SHIFT_MASK
+        mask = set_mask(mask, Gdk::Window::SHIFT_MASK)
       elsif p.downcase == 'mod1'
-        mask |= Gdk::Window::MOD1_MASK
+        mask = set_mask(mask, Gdk::Window::MOD1_MASK)
       elsif p.downcase == 'mod2'
-        mask |= Gdk::Window::MOD2_MASK
+        mask = set_mask(mask, Gdk::Window::MOD2_MASK)
       elsif p.downcase == 'mod3'
-        mask |= Gdk::Window::MOD3_MASK
+        mask = set_mask(mask, Gdk::Window::MOD3_MASK)
       else
         char = Gdk::Keyval.from_name(p)
       end
